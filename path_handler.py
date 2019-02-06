ef73f1038944f2388416484f1b6a47aaf30ace75
Zsofia Limbek <zsofia.limbek@gmail.com>
2019-02-06 15:25:38 +0100

    implemented path handling

---
+++ path_handler.py
@@ -0,0 +1,21 @@
+import os
+
+PATH_FILE = "path_to_dir.txt"
+
+
+def get_path():
+    with open(PATH_FILE) as f:
+        dir_path = f.read()
+    if not os.path.isdir(dir_path):
+        dir_path = get_path_input("Please enter the path to the folder that contains your pictures: ")
+    return dir_path
+
+
+def get_path_input(message):
+    user_input = input(message)
+
+    while not os.path.isdir(user_input):
+        print("Invalid path: ", user_input)
+        user_input = input(message)
+
+    return user_input
